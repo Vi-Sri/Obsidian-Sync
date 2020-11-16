218f8b8ffd44b6b396b7f5c8fd6fb392cc091f99
Vi-Sri <nikola.tesla195@gmail.com>
2020-11-16 18:49:10 +0530

    modifying tox env

--- obsidiansync/sync.py
+++ obsidiansync/sync.py
@@ -1,9 +1,10 @@
 import click
 import daemon
+import rumps
+import git
 
 
-
-def main(name, as_cowboy):
-    """A tool to have auto sync obsidian notes from your repo vault"""
-    greet = 'Howdy' if as_cowboy else 'Hello'
-    click.echo('{0}, {1}.'.format(greet, name))
+class AwesomeStatusBarApp(rumps.App):
+    def __init__(self):
+        super(AwesomeStatusBarApp, self).__init__("--")
+       
